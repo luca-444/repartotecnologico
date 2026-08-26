25d111c62830ca9996ed7ff214f4e1e61057b2d1
Luca <73776778+luca-444@users.noreply.github.com>
2026-08-26 14:20:28 +0200

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://repartotecnologico.it',
-  trailingSlash: 'ignore',
-  build: {
-    format: 'directory',
-  },
-});
